4b27a4b3e6c5ccea174678230b4a21bb04b90837
uncaha <cczxntken@163.com>
2018-04-25 14:20:50 +0800

    图集工具

---
+++ LitEngine/Script/Tool/SpriteArray.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+public class SpriteArray : MonoBehaviour
+{
+    private bool isinit = false;
+    private Dictionary<string, Sprite> spdic;
+    public Sprite[] sprites;
+
+    private void Init()
+    {
+        if (isinit) return;
+        if (sprites != null && sprites.Length > 0)
+        {
+            spdic = new Dictionary<string, Sprite>();
+            for (int i = 0; i < sprites.Length; i++)
+            {
+                spdic.Add(sprites[i].name, sprites[i]);
+            }
+        }
+        isinit = true;
+    }
+
+    public Sprite this[string _name]
+    {
+        get
+        {
+            if (!isinit) Init();
+            if (spdic == null || !spdic.ContainsKey(_name)) return null;
+            return spdic[_name];
+        }
+    }
+
+    private void OnDestroy()
+    {
+        spdic.Clear();
+    }
+}
