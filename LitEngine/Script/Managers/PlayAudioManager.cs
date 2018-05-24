04537b366015bc0627a9970a01b3c2427389579c
uncaha <cczxntken@163.com>
2018-05-24 15:36:24 +0800

    音效管理器添加AudioMixerGroup

--- LitEngine/Script/Managers/PlayAudioManager.cs
+++ LitEngine/Script/Managers/PlayAudioManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Audio;
 namespace LitEngine
 {
     public class PlayAudioManager : MonoManagerBase
@@ -15,6 +16,38 @@ namespace LitEngine
                     sInstance = tobj.AddComponent<PlayAudioManager>();
                 }
                 return sInstance;
+            }
+        }
+
+        private AudioMixerGroup _BackMusicMixer = null;
+        public static AudioMixerGroup BackMusicMixer
+        {
+            get {
+                return Instance._BackMusicMixer;
+            }
+            
+            set
+            {
+                Instance._BackMusicMixer = value;
+                Instance.mBackMusic.outputAudioMixerGroup = value;
+            }
+        }
+
+        private AudioMixerGroup _SoundMixer = null;
+        public static AudioMixerGroup SoundMixer
+        {
+            get
+            {
+                return Instance._SoundMixer;
+            }
+
+            set
+            {
+                Instance._SoundMixer = value;
+                for (int i = 0; i < Instance.mMaxSoundCount; i++)
+                {
+                    Instance.mAudioMixerSounds[i].outputAudioMixerGroup = value;
+                }
             }
         }
 
@@ -54,19 +87,32 @@ namespace LitEngine
                 {
                     Instance.mAudioSounds[i].volume = mSoundRealVolume;
                 }
+
+                for (int i = 0; i < Instance.mMaxSoundCount; i++)
+                {
+                    Instance.mAudioMixerSounds[i].volume = mSoundRealVolume;
+                }
             }
         }
 
         private int mMaxSoundCount = 3;
 
         private AudioSource mBackMusic;
+        private AudioSource[] mAudioMixerSounds;
         private AudioSource[] mAudioSounds;
         
         private int mIndex = 0;
-
+        private int mMixerIndex = 0;
         private void Awake()
         {
             mBackMusic = gameObject.AddComponent<AudioSource>();
+
+            mAudioMixerSounds = new AudioSource[mMaxSoundCount];
+            for (int i = 0; i < mMaxSoundCount; i++)
+            {
+                mAudioMixerSounds[i] = gameObject.AddComponent<AudioSource>();
+            }
+
             mAudioSounds = new AudioSource[mMaxSoundCount];
             for(int i = 0;i< mMaxSoundCount; i++)
             {
@@ -76,6 +122,16 @@ namespace LitEngine
         override protected void OnDestroy()
         {
             base.OnDestroy();
+        }
+
+        static public void PlayMixerSound(AudioClip _clip)
+        {
+            if (Instance.mMixerIndex == Instance.mMaxSoundCount) Instance.mMixerIndex = 0;
+            Instance.mAudioMixerSounds[Instance.mMixerIndex].Stop();
+            Instance.mAudioMixerSounds[Instance.mMixerIndex].clip = _clip;
+            Instance.mAudioMixerSounds[Instance.mMixerIndex].loop = false;
+            Instance.mAudioMixerSounds[Instance.mMixerIndex].Play();
+            Instance.mMixerIndex++;
         }
 
         static public void PlaySound(AudioClip _clip)
@@ -108,6 +164,11 @@ namespace LitEngine
             {
                 Instance.mAudioSounds[i].Stop();
             }
+
+            for (int i = 0; i < Instance.mMaxSoundCount; i++)
+            {
+                Instance.mAudioMixerSounds[i].Stop();
+            }
         }
 
         static public void Clear()
@@ -118,6 +179,12 @@ namespace LitEngine
             {
                 Instance.mAudioSounds[i].Stop();
                 Instance.mAudioSounds[i].clip = null;
+            }
+
+            for (int i = 0; i < Instance.mMaxSoundCount; i++)
+            {
+                Instance.mAudioMixerSounds[i].Stop();
+                Instance.mAudioMixerSounds[i].clip = null;
             }
         }
     }
