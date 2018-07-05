b54cd3c895a241686207fb701409d9bd064bde0a
uncaha <cczxntken@163.com>
2018-07-05 17:01:56 +0800

    添加一个批量下载类 DownLoadGroup.cs

--- LitEngine/Script/DownLoad/DownLoadObject.cs
+++ LitEngine/Script/DownLoad/DownLoadObject.cs
@@ -7,250 +7,248 @@ using System.Threading;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 
-namespace LitEngine
+namespace LitEngine.DownLoad
 {
-    namespace DownLoad
+    public class DownLoadObject : System.Collections.IEnumerator, System.IDisposable
     {
-        public class DownLoadObject:System.Collections.IEnumerator, System.IDisposable
+        #region 属性
+        public string DestinationPath { get; private set; }
+        public string SourceURL { get; private set; }
+        public string TempFile { get; private set; }
+        public string CompleteFile { get; private set; }
+        public string FileName { get; private set; }
+        public string Error { get; private set; }
+        public float Progress
         {
-            #region 属性
-            public string DestinationPath { get; private set; }
-            public string SourceURL { get; private set; }
-            public string TempFile { get; private set; }
-            public string CompleteFile { get; private set; }
-            public string FileName { get; private set; }
-            public string Error { get; private set; }
-            public float Progress
+            get
             {
-                get
+                return ContentLength > 0 ? (float)DownLoadedLength / ContentLength : 0;
+            }
+        }
+
+        public bool IsDone
+        {
+            get
+            {
+                if (mIsDone) return true;
+                return false;
+            }
+        }
+
+        public object Current { get; }
+
+        public bool MoveNext()
+        {
+            return !IsDone;
+        }
+        public void Reset()
+        {
+
+        }
+
+        public long ContentLength { get; private set; }
+        public long DownLoadedLength { get; private set; }
+
+        private bool mIsClear = false;
+        private bool mIsDone = false;
+
+        private bool mIsStart = false;
+        private bool mThreadRuning = false;
+
+        private HttpWebRequest mReqest;
+        private WebResponse mResponse;
+        private Stream mHttpStream;
+
+        private Thread mDownLoadThread = null;
+
+        #endregion
+        #region 构造析构
+        public DownLoadObject(string _sourceurl, string _destination, bool _clear)
+        {
+            SourceURL = _sourceurl;
+            DestinationPath = _destination;
+            mIsClear = _clear;
+
+            Error = null;
+
+            string[] turlstrs = SourceURL.Split('/');
+            FileName = turlstrs[turlstrs.Length - 1];
+
+            TempFile = DestinationPath + "/" + FileName + ".temp";
+            CompleteFile = DestinationPath + "/" + FileName;
+        }
+
+        ~DownLoadObject()
+        {
+            Dispose(false);
+        }
+
+        private bool mDisposed = false;
+        public void Dispose()
+        {
+            Dispose(true);
+            System.GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool _disposing)
+        {
+            if (mDisposed)
+                return;
+            mDisposed = true;
+
+            mThreadRuning = false;
+
+            if (mDownLoadThread != null)
+                mDownLoadThread.Join();
+
+            CloseHttpClient();
+        }
+        #endregion
+        public void StartDownLoadAsync()
+        {
+            if (mIsStart) return;
+            mIsStart = true;
+
+            mThreadRuning = true;
+            mDownLoadThread = new Thread(ReadNetByte);
+            mDownLoadThread.IsBackground = true;
+            mDownLoadThread.Start();
+        }
+
+        public void StartDownLoad()
+        {
+            if (mIsStart) return;
+            mIsStart = true;
+
+            mThreadRuning = true;
+            ReadNetByte();
+        }
+
+        private void ReadNetByte()
+        {
+            FileStream ttempfile = null;
+            try
+            {
+                long thaveindex = 0;
+                if (File.Exists(TempFile))
                 {
-                    return ContentLength > 0 ?(float)DownLoadedLength / ContentLength : 0;
-                }
-            }
 
-            public bool IsDone
-            {
-                get {
-                    if (mIsDone) return true;
-                    return false;
-                }
-            }
-
-            public object Current { get; }
-
-            public bool MoveNext()
-            {
-                return !IsDone;
-            }
-            public void Reset()
-            {
-
-            }
-
-            public long ContentLength { get; private set; }
-            public long DownLoadedLength { get; private set; }
-
-            private bool mIsClear = false;
-            private bool mIsDone = false;
-
-            private bool mIsStart = false;
-            private bool mThreadRuning = false;
-
-            private HttpWebRequest mReqest;
-            private WebResponse mResponse;
-            private Stream mHttpStream;
-
-            private Thread mDownLoadThread = null;
-
-            #endregion
-            #region 构造析构
-            public DownLoadObject(string _sourceurl, string _destination, bool _clear)
-            {
-                SourceURL = _sourceurl;
-                DestinationPath = _destination;
-                mIsClear = _clear;
-
-                Error = null;
-
-                string[] turlstrs = SourceURL.Split('/');
-                FileName = turlstrs[turlstrs.Length - 1];
-
-                TempFile = DestinationPath + "/" + FileName + ".temp";
-                CompleteFile = DestinationPath + "/" + FileName;
-            }
-
-            ~DownLoadObject()
-            {
-                Dispose(false);
-            }
-
-            private bool mDisposed = false;
-            public void Dispose()
-            {
-                Dispose(true);
-                System.GC.SuppressFinalize(this);
-            }
-
-            private void Dispose(bool _disposing)
-            {
-                if (mDisposed)
-                    return;
-                mDisposed = true;
-
-                mThreadRuning = false;
-
-                if (mDownLoadThread != null)
-                    mDownLoadThread.Join();
-
-                CloseHttpClient();
-            }
-            #endregion
-            public void StartDownLoadAsync()
-            {
-                if (mIsStart) return;
-                mIsStart = true;
-
-                mThreadRuning = true;
-                mDownLoadThread = new Thread(ReadNetByte);
-                mDownLoadThread.IsBackground = true;
-                mDownLoadThread.Start();
-            }
-
-            public void StartDownLoad()
-            {
-                if (mIsStart) return;
-                mIsStart = true;
-
-                mThreadRuning = true;
-                ReadNetByte();
-            }
-
-            private void ReadNetByte()
-            {
-                FileStream ttempfile = null;
-                try
-                {
-                    long thaveindex = 0;
-                    if (File.Exists(TempFile))
+                    if (!mIsClear)
                     {
-                        
-                        if (!mIsClear)
-                        {
-                            ttempfile = System.IO.File.OpenWrite(TempFile);
-                            thaveindex = ttempfile.Length;
-                            ttempfile.Seek(thaveindex, SeekOrigin.Current);
-                        }
-                        else
-                        {
-                            File.Delete(TempFile);
-                            thaveindex = 0;
-                        }
-
+                        ttempfile = System.IO.File.OpenWrite(TempFile);
+                        thaveindex = ttempfile.Length;
+                        ttempfile.Seek(thaveindex, SeekOrigin.Current);
+                    }
+                    else
+                    {
+                        File.Delete(TempFile);
+                        thaveindex = 0;
                     }
 
-                    mReqest = (HttpWebRequest)HttpWebRequest.Create(SourceURL);
-                    mReqest.Timeout = 5000;
+                }
 
-                    if (SourceURL.Contains("https://"))
-                        ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+                mReqest = (HttpWebRequest)HttpWebRequest.Create(SourceURL);
+                mReqest.Timeout = 5000;
 
-                    if (thaveindex > 0)
-                        mReqest.AddRange((int)thaveindex);
+                if (SourceURL.Contains("https://"))
+                    ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
 
-                    int tindex = 0;
-                    while(tindex++ < 4 && mThreadRuning)
+                if (thaveindex > 0)
+                    mReqest.AddRange((int)thaveindex);
+
+                int tindex = 0;
+                while (tindex++ < 4 && mThreadRuning)
+                {
+                    try
                     {
-                        try
-                        {
-                            mResponse = mReqest.GetResponse();
-                            break;
-                        }
-                        catch (System.Exception _error)
-                        {
-                            DLog.LogFormat("ReadNetByte 获取Response失败,尝试次数{0},error = {1}", tindex, _error);
-                        }
+                        mResponse = mReqest.GetResponse();
+                        break;
                     }
+                    catch (System.Exception _error)
+                    {
+                        DLog.LogFormat("ReadNetByte 获取Response失败,尝试次数{0},error = {1}", tindex, _error);
+                    }
+                }
 
-                    if (mResponse == null)
-                        throw new System.NullReferenceException("ReadNetByte 获取Response失败.");
-                    
-                    mHttpStream = mResponse.GetResponseStream();
-                    ContentLength = mResponse.ContentLength;
+                if (mResponse == null)
+                    throw new System.NullReferenceException("ReadNetByte 获取Response失败.");
 
-                    if (ttempfile == null)
-                        ttempfile = System.IO.File.Create(TempFile);
+                mHttpStream = mResponse.GetResponseStream();
+                ContentLength = mResponse.ContentLength;
 
-                    int tcount = 0;
-                    int tlen = 1024;
-                    byte[] tbuffer = new byte[tlen];
-                    int tReadSize = 0;
+                if (ttempfile == null)
+                    ttempfile = System.IO.File.Create(TempFile);
+
+                int tcount = 0;
+                int tlen = 1024;
+                byte[] tbuffer = new byte[tlen];
+                int tReadSize = 0;
+                tReadSize = mHttpStream.Read(tbuffer, 0, tlen);
+                while (tReadSize > 0 && mThreadRuning)
+                {
+                    DownLoadedLength += tReadSize;
+                    ttempfile.Write(tbuffer, 0, tReadSize);
                     tReadSize = mHttpStream.Read(tbuffer, 0, tlen);
-                    while (tReadSize > 0 && mThreadRuning)
+
+                    if (++tcount >= 512)
                     {
-                        DownLoadedLength += tReadSize;
-                        ttempfile.Write(tbuffer, 0, tReadSize);
-                        tReadSize = mHttpStream.Read(tbuffer, 0, tlen);
-
-                        if (++tcount>= 512)
-                        {
-                            ttempfile.Flush();
-                            tcount = 0;
-                        }
-                        
+                        ttempfile.Flush();
+                        tcount = 0;
                     }
-                }
-                catch(System.Exception _error)
-                {
-                    Error = _error.ToString();
-                }
 
-                if (ttempfile != null)
-                    ttempfile.Close();
+                }
+            }
+            catch (System.Exception _error)
+            {
+                Error = _error.ToString();
+            }
 
-                if (DownLoadedLength == ContentLength)
+            if (ttempfile != null)
+                ttempfile.Close();
+
+            if (DownLoadedLength == ContentLength)
+            {
+                if (File.Exists(TempFile))
                 {
-                    if (File.Exists(TempFile))
+                    if (File.Exists(CompleteFile))
                     {
-                        if (File.Exists(CompleteFile))
-                        {
-                            File.Delete(CompleteFile);
-                        }
-                        File.Move(TempFile, CompleteFile);
+                        File.Delete(CompleteFile);
                     }
+                    File.Move(TempFile, CompleteFile);
                 }
-                else
-                {
-                    if(Error == null)
-                        Error = "文件未能完成下载.Stream 被中断.";
-                }
-
-                CloseHttpClient();
-                mIsDone = true;
             }
-            private static bool CheckValidationResult(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
+            else
             {
-                return true;
+                if (Error == null)
+                    Error = "文件未能完成下载.Stream 被中断.";
             }
-            private void CloseHttpClient()
+
+            CloseHttpClient();
+            mIsDone = true;
+        }
+        private static bool CheckValidationResult(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
+        {
+            return true;
+        }
+        private void CloseHttpClient()
+        {
+            if (mHttpStream != null)
             {
-                if (mHttpStream != null)
-                {
-                    mHttpStream.Close();
-                    mHttpStream.Dispose();
-                    mHttpStream = null;
-                }
+                mHttpStream.Close();
+                mHttpStream.Dispose();
+                mHttpStream = null;
+            }
 
-                if(mResponse != null)
-                {
-                    mResponse.Close();
-                    mResponse = null;
-                }
+            if (mResponse != null)
+            {
+                mResponse.Close();
+                mResponse = null;
+            }
 
-                if (mReqest != null)
-                {
-                    mReqest.Abort();
-                    mReqest = null;
-                }
+            if (mReqest != null)
+            {
+                mReqest.Abort();
+                mReqest = null;
             }
         }
     }
---
+++ LitEngine/Script/Task/DownLoadGroup.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using LitEngine.UpdateSpace;
+namespace LitEngine.DownLoad
+{
+
+    public class DownLoadGroup : System.Collections.IEnumerator, IDisposable
+    {
+        public long ContentLength { get; private set; }
+        public long DownLoadedLength { get; private set; }
+        public float Progress
+        {
+            get
+            {
+                return ContentLength > 0 ? (float)DownLoadedLength / ContentLength : 0;
+            }
+        }
+
+        public bool IsDone { get; private set; }
+        public bool IsStart { get; private set; }
+        public string Key { get; private set; }
+
+        private bool autoDispose = false;
+        private Action<string[], string> FinishedDelegate;
+        private Action<long, long, float> ProgressDelegate;
+        private List<DownLoadObject> groupList = new List<DownLoadObject>();
+        private UpdateNeedDisObject mUpdateObject;
+        public DownLoadGroup(string newKey ,bool newAutoDispose = true)
+        {
+            Key = newKey;
+            autoDispose = newAutoDispose;
+            mUpdateObject = new UpdateNeedDisObject(Key, Update, Dispose);
+        }
+        ~DownLoadGroup()
+        {
+            Dispose(false);
+        }
+
+        #region dispose
+        protected bool mDisposed = false;
+        public void Dispose()
+        {
+            Dispose(true);
+            System.GC.SuppressFinalize(this);
+        }
+
+        protected void Dispose(bool _disposing)
+        {
+            if (mDisposed)
+                return;
+            mDisposed = true;
+            DisposeGC();
+        }
+
+        protected void DisposeGC()
+        {
+            for (int i = 0; i < groupList.Count; i++)
+            {
+                groupList[i].Dispose();
+            }
+            groupList.Clear();
+            mUpdateObject.UnRegToOwner();
+            mUpdateObject = null;
+            FinishedDelegate = null;
+            ProgressDelegate = null;
+        }
+        #endregion
+
+        #region ito
+        public object Current { get; }
+
+        public bool MoveNext()
+        {
+            return !IsDone;
+        }
+        public void Reset()
+        {
+
+        }
+        #endregion
+
+        public void AddByUrl(string _sourceurl, string _destination, bool _clear)
+        {
+            Add(new DownLoadObject(_sourceurl, _destination, _clear));
+        }
+
+        public void Add(DownLoadObject newObject)
+        {
+            if(IsStart)
+            {
+                DLog.LogError("已经开始的任务不可插入新内容." );
+                return;
+            }
+            if (groupList.Contains(newObject))
+            {
+                DLog.LogError("重复下载,url = "+newObject.SourceURL);
+                return;
+            }
+            groupList.Add(newObject);
+        }
+
+        public void StartAsync()
+        {
+            if (IsStart) return;
+            IsStart = true;
+
+            for (int i = 0; i < groupList.Count; i++)
+            {
+                groupList[i].StartDownLoadAsync();
+            }
+
+            PublicUpdateManager.AddUpdate(mUpdateObject);
+        }
+
+        bool isAllDone = false;
+        protected void Update()
+        {
+            if (IsDone) return;
+
+            ContentLength = 0;
+            DownLoadedLength = 0;
+            isAllDone = true;
+            for (int i = 0; i < groupList.Count; i++)
+            {
+                ContentLength += groupList[i].ContentLength;
+                DownLoadedLength += groupList[i].DownLoadedLength;
+                if (!groupList[i].IsDone)
+                    isAllDone = false;
+            }
+            IsDone = isAllDone;
+
+            if (ProgressDelegate != null)
+                ProgressDelegate(DownLoadedLength,ContentLength, Progress);
+
+            if (IsDone)
+            {
+                string[] tcmpfile = new string[groupList.Count];
+                string terror = "";
+                for (int i = 0; i < groupList.Count; i++)
+                {
+                    tcmpfile[i] = groupList[i].CompleteFile;
+                    terror += groupList[i].Error == null ? "" : groupList[i].Error + "|";
+                }
+
+                Action<string[], string> tfinished = FinishedDelegate;
+
+                if (autoDispose)
+                    Dispose();
+                else
+                    mUpdateObject.UnRegToOwner();
+
+                try
+                {
+                    if (tfinished != null)
+                        tfinished(tcmpfile, terror);
+                }
+                catch (System.Exception _error)
+                {
+                    DLog.LogError(_error);
+                }
+            }
+        }
+    }
+}
