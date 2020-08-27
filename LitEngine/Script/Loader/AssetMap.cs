d3d0e2c06ac816a19e5301b754d064db0ff060df
uncaha <cczxntken@habby.com>
2020-08-27 17:07:19 +0800

    fixed

---
+++ LitEngine/Script/Loader/AssetMap.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+namespace LitEngine.LoadAsset
+{
+    [CreateAssetMenu(fileName = "AssetMap", menuName = "AssetTool/AssetMap", order =1)]
+    public class AssetMap : ScriptableObject
+    {
+        [System.Serializable]
+        public class AssetObject
+        {
+            public string assetName;
+            public string sufixx;
+            public bool isInSide;
+            public AssetObject(string pFileName)
+            {
+                int tindex = pFileName.LastIndexOf('.');
+                if(tindex > -1)
+                {
+                    sufixx = pFileName.Substring(tindex,pFileName.Length - tindex);
+                    assetName = pFileName.Replace(sufixx,"");
+                }
+                else
+                {
+                    sufixx = "";
+                    assetName = pFileName;
+                }
+                    
+                
+            }
+        }
+        
+        public AssetObject[] assets;
+
+        private static AssetObject sNullObject;
+        private Dictionary<string,AssetObject> assetMap;
+        
+       [System.NonSerialized] bool inited = false;
+        public void Init()
+        {
+            if(inited) return;
+            inited = true;
+            if(assets == null)
+            {
+                assets = new AssetObject[0];
+            }
+            sNullObject = new AssetObject("Null");
+            assetMap  = new Dictionary<string,AssetObject>(assets.Length);
+            foreach(var item in assets)
+            {
+                try
+                {
+                    assetMap.Add(item.assetName,item);
+                }
+                catch (System.Exception e)
+                {
+                    Debug.LogErrorFormat("asset = {0} error = {1}" ,item.assetName, e.Message);
+                }
+            }
+        }
+
+        public AssetObject GetAsset(string pAsset)
+        {
+            if (assetMap == null)
+            {
+                Init();
+            }
+
+            AssetObject ret;
+            if (!assetMap.TryGetValue(pAsset, out ret))
+            {
+                ret = sNullObject;
+                ret.assetName = pAsset;
+                ret.isInSide = true;
+            }
+            return ret;
+        }
+    }
+    
+}
