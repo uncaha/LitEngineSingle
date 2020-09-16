eeeb7ed9fe56f2a0cbf02dc8d1f9372f1b3d6768
uncaha <cczxntken@habby.com>
2020-09-16 20:14:24 +0800

    fixed

--- LitEngine/Script/Loader/LoadTool/LoadGroup.cs
+++ LitEngine/Script/Loader/LoadTool/LoadGroup.cs
@@ -29,21 +29,108 @@ namespace LitEngine.LoadAsset
     {
         public class LoadAssetObject
         {
-            public string assetName;
-            public System.Action<string, object> onComplete;
+            public string assetName { get; private set; }
+            public string groupName { get; private set; }
+            public bool isLoaded { get; private set; }
+            System.Action<string, object> onComplete;
+            System.Action<LoadAssetObject> onCompleteParent;
+
+            private bool isStart = false;
+            public LoadAssetObject(string pName, System.Action<string, object> pOnComplete, System.Action<LoadAssetObject> pParentCall, string pGroup)
+            {
+                assetName = pName;
+                onComplete = pOnComplete;
+                onCompleteParent = pParentCall;
+                groupName = pGroup;
+            }
+            public void Start()
+            {
+                if (isStart) return;
+                isStart = true;
+                LoaderManager.LoadAssetAsync(assetName, assetName, LoadComplete, groupName);
+            }
+
+            void LoadComplete(string pKey, object pRes)
+            {
+                isLoaded = true;
+                try
+                {
+                    onComplete?.Invoke(pKey, pRes);
+                }
+                catch (System.Exception error)
+                {
+                    DLog.LogErrorFormat("asset = {0},error = {1}", assetName, error.Message);
+                }
+                onCompleteParent?.Invoke(this);
+            }
         }
         public string Key { get; private set; }
-
-        private List<LoadAssetObject> assetList = new List<LoadAssetObject>();
+        public bool IsStart { get; private set; }
+        public int CompleteCount { get; private set; }
+        public int Count { get { return waitLoadList.Count; } }
+        public event System.Action<LoadGroup> onComplete;
+        private List<LoadAssetObject> waitLoadList = new List<LoadAssetObject>();
+        private bool isCalledComplete = false;
         public LoadGroup(string pkey)
         {
             Key = pkey;
+            IsStart = false;
         }
 
         public void Add(string pAssetName, System.Action<string, object> pComplete)
         {
-            var tobj = new LoadAssetObject(){assetName = pAssetName,onComplete = pComplete};
-            assetList.Add(tobj);
+            if (IsStart)
+            {
+                DLog.LogErrorFormat("Group已经开始加载.添加失败.name = {0}", pAssetName);
+                return;
+            }
+            var tobj = new LoadAssetObject(pAssetName, pComplete, OnAssetLoaded, Key);
+            waitLoadList.Add(tobj);
+        }
+
+        private void OnAssetLoaded(LoadAssetObject pSender)
+        {
+            CompleteCount++;
+            CallComplete();
+        }
+
+        void CallComplete()
+        {
+            if (isCalledComplete) return;
+            if (CompleteCount == waitLoadList.Count)
+            {
+                isCalledComplete = true;
+                try
+                {
+                    onComplete?.Invoke(this);
+                }
+                catch (System.Exception error)
+                {
+                    DLog.LogErrorFormat("group = {0},error = {1}", Key, error.Message);
+                    DLog.LogError(error.ToString());
+                }
+            }
+        }
+
+        public void StartLoad()
+        {
+            if (IsStart) return;
+            IsStart = true;
+            isCalledComplete = false;
+            CompleteCount = 0;
+            for (int i = waitLoadList.Count - 1; i >= 0; i--)
+            {
+                var item = waitLoadList[i];
+                if (!item.isLoaded)
+                {
+                    item.Start();
+                }
+                else
+                {
+                    CompleteCount++;
+                }
+
+            }
         }
     }
 }
