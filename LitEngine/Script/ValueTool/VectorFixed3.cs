ac1bc7c22dcaf1c237fc51b56cda086651d487ee
uncaha <cczxntken@habby.com>
2021-03-01 16:58:11 +0800

    add valuetype

---
+++ LitEngine/Script/ValueTool/VectorFixed3.cs
@@ -0,0 +1,224 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+namespace LitEngine.Value
+{
+    public struct VectorFixed3
+    {
+
+        public Fixed x
+        {
+            get;
+            private set;
+        }
+        public Fixed y
+        {
+            get;
+            private set;
+        }
+        public Fixed z
+        {
+            get;
+            private set;
+        }
+
+        #region 属性
+        public static VectorFixed3 left = new VectorFixed3(-1, 0);
+        public static VectorFixed3 right = new VectorFixed3(1, 0);
+        public static VectorFixed3 up = new VectorFixed3(0, 1);
+        public static VectorFixed3 down = new VectorFixed3(0, -1);
+        public static VectorFixed3 zero = new VectorFixed3(0, 0, 0);
+        public static VectorFixed3 one = new VectorFixed3(1, 1, 1);
+        public static VectorFixed3 forward = new VectorFixed3(0, 0, 1);
+        #endregion
+
+        public VectorFixed3(int x = 0, int y = 0, int z = 0)
+        {
+            this.x = new Fixed(x);
+            this.y = new Fixed(y);
+            this.z = new Fixed(z);
+        }
+        public VectorFixed3(float x, float y, float z)
+        {
+            this.x = new Fixed(x);
+            this.y = new Fixed(y);
+            this.z = new Fixed(z);
+        }
+        public VectorFixed3(Fixed x, Fixed y, Fixed z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public VectorFixed3(Vector3 v3)
+        {
+            this.x = new Fixed(v3.x);
+            this.y = new Fixed(v3.y);
+            this.z = new Fixed(v3.z);
+        }
+        public Vector3 ToVector3()
+        {
+            return new Vector3(x.ToFloat(), y.ToFloat(),z.ToFloat());
+        }
+
+        public static VectorFixed3 FromVector3(Vector3 v3)
+        {
+            return new VectorFixed3(v3);
+        }
+
+        #region 重载
+        public static VectorFixed3 operator +(VectorFixed3 a, VectorFixed3 b)
+        {
+            return new VectorFixed3(a.x + b.x, a.y + b.y, a.z + b.z);
+        }
+        public static VectorFixed3 operator +(VectorFixed3 a, Vector3 b)
+        {
+            return new VectorFixed3(a.x + b.x, a.y + b.y, a.z + b.z);
+        }
+        public static VectorFixed3 operator -(VectorFixed3 a, VectorFixed3 b)
+        {
+            return new VectorFixed3(a.x - b.x, a.y - b.y, a.z - b.z);
+        }
+        public static VectorFixed3 operator -(VectorFixed3 a, Vector3 b)
+        {
+            return new VectorFixed3(a.x - b.x, a.y - b.y, a.z - b.z);
+        }
+        public static VectorFixed3 operator -(VectorFixed3 a)
+        {
+            return new VectorFixed3(-a.x, -a.y, -a.z);
+        }
+        public static VectorFixed3 operator *(VectorFixed3 a, Fixed b)
+        {
+            return new VectorFixed3(a.x * b, a.y * b,a.z * b);
+        }
+        public static VectorFixed3 operator *(VectorFixed3 a, int b)
+        {
+            return new VectorFixed3(a.x * b, a.y * b, a.z * b);
+        }
+        public static VectorFixed3 operator *(VectorFixed3 a, float b)
+        {
+            return new VectorFixed3(a.x * b, a.y * b, a.z * b);
+        }
+
+        public static VectorFixed3 operator *(VectorFixed3 a, VectorFixed3 b)
+        {
+            return new VectorFixed3(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
+        }
+        public static VectorFixed3 operator *(VectorFixed3 a, Vector3 b)
+        {
+            return new VectorFixed3(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
+        }
+        #endregion
+
+        #region 方法
+
+        public VectorFixed3 normalized
+        {
+            get
+            {
+                if (x == 0 && y == 0)
+                {
+                    return VectorFixed3.zero;
+                }
+                Fixed n = ((x * x) + (y * y) + (z * z)).Sqrt();
+
+                var result = new VectorFixed3(x / n, y / n, z / n);
+                result.x = Fixed.Range(result.x, -1, 1);
+                result.y = Fixed.Range(result.y, -1, 1);
+                result.z = Fixed.Range(result.z, -1, 1);
+                return result;
+            }
+        }
+
+        public Fixed magnitude
+        {
+
+            get
+            {
+                if (x == 0 && y == 0)
+                {
+                    return Fixed.Zero;
+                }
+                Fixed n = ((x * x) + (y * y) + (z * z)).Sqrt();
+                return n;
+            }
+        }
+
+        public Fixed Distance(VectorFixed3 b)
+        {
+            return (this - b).magnitude;
+        }
+
+        public Fixed Angle(VectorFixed3 b)
+        {
+            return Angle(this,b);
+        }
+
+        public Fixed Angle(Vector3 b)
+        {
+            return Angle(this, b);
+        }
+
+        public static Fixed Angle(VectorFixed3 a,VectorFixed3 b)
+        {
+            var tan = a.normalized;
+            var tbn = b.normalized;
+
+            return Dot(tan, tbn);
+        }
+        public static Fixed Angle(VectorFixed3 a, Vector3 b)
+        {
+            var tan = a.normalized;
+            var tbn = b.normalized;
+
+            return Dot(tan, tbn);
+        }
+
+        #region dot
+        public Fixed Dot(VectorFixed3 b)
+        {
+            return Dot(this, b);
+        }
+        public Fixed Dot(Vector3 b)
+        {
+            return Dot(this, b);
+        }
+        public static Fixed Dot(VectorFixed3 a, VectorFixed3 b)
+        {
+            return a.x * b.x + b.y * a.y + a.z * b.z;
+        }
+        public static Fixed Dot(VectorFixed3 a, Vector3 b)
+        {
+            return a.x * b.x + b.y * a.y + a.z * b.z;
+        }
+        #endregion
+
+        #region cross
+        public VectorFixed3 Cross(VectorFixed3 b)
+        {
+            return Cross(this, b);
+        }
+
+        public VectorFixed3 Cross(Vector3 b)
+        {
+            return Cross(this, b);
+        }
+
+        public static VectorFixed3 Cross(VectorFixed3 a, VectorFixed3 b)
+        {
+            return a * b;
+        }
+        public static VectorFixed3 Cross(VectorFixed3 a, Vector3 b)
+        {
+            return a * b;
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            return string.Format("{{0},{1},{2}}",x,y,z);
+        }
+        #endregion
+
+    }
+}
