2e19f4cd340883fc4b2fa822475d594c13ab7ce2
uncaha <yangzhanying@habby.com>
2022-09-23 21:52:00 +0800

    Update SQLLog.cs

--- LitEngine/Script/SQL/SQLLog.cs
+++ LitEngine/Script/SQL/SQLLog.cs
@@ -8,38 +8,38 @@ namespace LitEngine.SQL
 
         public static void Log(object pobj)
         {
-            Debug.Log($"[{LogTag}] {pobj}");
+            DLog.Log($"[{LogTag}] {pobj}");
         }
 
         public static void LogWarning(object pobj)
         {
-            Debug.LogWarning($"[{LogTag}] {pobj}");
+            DLog.LogWarning($"[{LogTag}] {pobj}");
         }
 
         public static void LogError(object pobj)
         {
-            Debug.LogError($"[{LogTag}] {pobj}");
+            DLog.LogError($"[{LogTag}] {pobj}");
         }
 
         public static void LogAssertion(object pobj)
         {
-            Debug.LogAssertion($"[{LogTag}] {pobj}");
+            DLog.LogAssertion($"[{LogTag}] {pobj}");
         }
 
 
         public static void LogFormat(string format, params object[] args)
         {
-            Debug.LogFormat($"[{ LogTag}] " + format, args);
+            DLog.LogFormat($"[{ LogTag}] " + format, args);
 
         }
         public static void LogWarningFormat(string format, params object[] args)
         {
-            Debug.LogWarningFormat($"[{ LogTag}] " + format, args);
+            DLog.LogWarningFormat($"[{ LogTag}] " + format, args);
         }
 
         public static void LogErrorFormat(string format, params object[] args)
         {
-            Debug.LogErrorFormat($"[{ LogTag}] " + format, args);
+            DLog.LogErrorFormat($"[{ LogTag}] " + format, args);
         }
     }
 }
