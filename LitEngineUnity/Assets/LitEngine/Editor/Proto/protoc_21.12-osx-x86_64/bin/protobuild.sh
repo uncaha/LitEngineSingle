9f4373f9d3c4e3f7729e871f602dc1a39773acac
uncaha <yangzhanying@habby.com>
2023-03-07 15:08:46 +0800

    add

---
+++ LitEngineUnity/Assets/LitEngine/Editor/Proto/protoc_21.12-osx-x86_64/bin/protobuild.sh
@@ -0,0 +1,4 @@
+
+./protoc --csharp_out=../../HabbyAccount/Assets/HabbySDK/HabbyGameCore/Runtime/interface/Data/protocs proto/ActionProtoRequest.proto
+./protoc --csharp_out=../../HabbyAccount/Assets/HabbySDK/HabbyGameCore/Runtime/interface/Data/protocs proto/ActionProtoResponse.proto
+./protoc --csharp_out=../../HabbyAccount/Assets/HabbySDK/HabbyGameCore/Runtime/interface/Data/protocs proto/GameCoreAny.proto
