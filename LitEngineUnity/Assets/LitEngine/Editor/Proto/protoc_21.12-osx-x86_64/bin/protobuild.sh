379f025681887cb42d4510449690539e41fcc0fa
uncaha <34293621+cczxntken@users.noreply.github.com>
2024-07-25 11:23:15 +0800

    add project

--- LitEngineUnity/Assets/LitEngine/Editor/Proto/protoc_21.12-osx-x86_64/bin/protobuild.sh
+++
@@ -1,4 +0,0 @@
-
-./protoc --csharp_out=../../HabbyAccount/Assets/HabbySDK/HabbyGameCore/Runtime/interface/Data/protocs proto/ActionProtoRequest.proto
-./protoc --csharp_out=../../HabbyAccount/Assets/HabbySDK/HabbyGameCore/Runtime/interface/Data/protocs proto/ActionProtoResponse.proto
-./protoc --csharp_out=../../HabbyAccount/Assets/HabbySDK/HabbyGameCore/Runtime/interface/Data/protocs proto/GameCoreAny.proto
