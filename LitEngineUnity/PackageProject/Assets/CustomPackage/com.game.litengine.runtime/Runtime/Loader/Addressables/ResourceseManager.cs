8b8799b491b668e99431cd406ac9b56cf02eede3
uncaha <34293621+cczxntken@users.noreply.github.com>
2024-07-26 16:12:16 +0800

    Update ResourceseManager.cs

--- LitEngineUnity/PackageProject/Assets/CustomPackage/com.game.litengine.runtime/Runtime/Loader/Addressables/ResourceseManager.cs
+++ LitEngineUnity/PackageProject/Assets/CustomPackage/com.game.litengine.runtime/Runtime/Loader/Addressables/ResourceseManager.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
-
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
@@ -12,10 +14,83 @@ namespace LitEngine.Addressable
         {
 
         }
-        
-        public AsyncOperationHandle<IList<TObject>> LoadAssetsAsync<TObject>(IEnumerable keys, Action<TObject> callback, Addressables.MergeMode mode, bool releaseDependenciesOnFailure)
+
+        static public AsyncOperationHandle<IList<TObject>> LoadAssetsAsync<TObject>(IEnumerable keys, Action<TObject> callback,
+            Addressables.MergeMode mode = Addressables.MergeMode.Union, bool releaseDependenciesOnFailure = true)
         {
-            return Addressables.LoadAssetsAsync<TObject>(keys, callback, mode, releaseDependenciesOnFailure);
+            var thandle = Addressables.LoadAssetsAsync(keys, callback, mode, releaseDependenciesOnFailure);
+            thandle.Completed += (handle) =>
+            {
+                if (handle.Status != AsyncOperationStatus.Succeeded)
+                {
+                    DLog.LogError("[ResourcesManager] LoadAssetsAsync Failed.");
+                }
+            };
+            return thandle;
         }
+        
+        static public AsyncOperationHandle<GameObject> InstantiateAssetAsync(object key, Transform parent = null, bool instantiateInWorldSpace = false, bool trackHandle = true)
+        {
+            var thandle = Addressables.InstantiateAsync(key, parent, instantiateInWorldSpace, trackHandle);
+            thandle.Completed += (handle) =>
+            {
+                if (handle.Status != AsyncOperationStatus.Succeeded)
+                {
+                    DLog.LogError("[ResourcesManager] InstantiateAssetAsync Failed.");
+                }
+            };
+            return thandle;
+        }
+        
+        /// <summary>
+        /// Release asset.
+        /// </summary>
+        /// <typeparam name="TObject">The type of the object being released</typeparam>
+        /// <param name="obj">The asset to release.</param>
+        public static void Release<TObject>(TObject obj)
+        {
+            Addressables.Release(obj);
+        }
+
+        /// <summary>
+        /// Release the operation and its associated resources.
+        /// </summary>
+        /// <typeparam name="TObject">The type of the AsyncOperationHandle being released</typeparam>
+        /// <param name="handle">The operation handle to release.</param>
+        public static void Release<TObject>(AsyncOperationHandle<TObject> handle)
+        {
+            Addressables.Release(handle);
+        }
+        
+        /// <summary>
+        /// Release the operation and its associated resources.
+        /// </summary>
+        /// <param name="handle">The operation handle to release.</param>
+        public static void Release(AsyncOperationHandle handle)
+        {
+            Addressables.Release(handle);
+        }
+
+        /// <summary>
+        /// Releases and destroys an object that was created via Addressables.InstantiateAsync.
+        /// </summary>
+        /// <param name="instance">The GameObject instance to be released and destroyed.</param>
+        /// <returns>Returns true if the instance was successfully released.</returns>
+        public static bool ReleaseInstance(GameObject instance)
+        {
+            return Addressables.ReleaseInstance(instance);
+        }
+
+        /// <summary>
+        /// Releases and destroys an object that was created via Addressables.InstantiateAsync.
+        /// </summary>
+        /// <param name="handle">The handle to the game object to destroy, that was returned by InstantiateAsync.</param>
+        /// <returns>Returns true if the instance was successfully released.</returns>
+        public static bool ReleaseInstance(AsyncOperationHandle handle)
+        {
+            Addressables.Release(handle);
+            return true;
+        }
+        
     }
 }
