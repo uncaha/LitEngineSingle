9e7cac235d24536320c1667bc007d8e4bbdd9ad7
uncaha <34293621+cczxntken@users.noreply.github.com>
2024-07-26 16:31:37 +0800

    Update ResourceseManager.cs

--- LitEngineUnity/PackageProject/Assets/CustomPackage/com.game.litengine.runtime/Runtime/Loader/Addressables/ResourceseManager.cs
+++ LitEngineUnity/PackageProject/Assets/CustomPackage/com.game.litengine.runtime/Runtime/Loader/Addressables/ResourceseManager.cs
@@ -15,6 +15,48 @@ namespace LitEngine.Addressable
 
         }
 
+         /// <summary>
+        /// Loads multiple assets, identified by a set of keys.
+        /// </summary>
+        /// <remarks>
+        /// The keys in <paramref name="keys"/> are translated into lists of locations, which are merged into a single list based on
+        /// the value in <paramref name="mode"/>.
+        ///
+        /// When you load Addressable assets, the system:
+        /// * Gathers the dependencies of the assets
+        /// * Downloads any remote AssetBundles needed to load the assets or their dependencies
+        /// * Loads the AssetBundles into memory
+        /// * Populates the `Result` object of the <see cref="AsyncOperationHandle{TObject}"/> instance returned by this function
+        ///
+        /// Use the `Result` object to access the loaded assets.
+        ///
+        /// If any assets cannot be loaded, the entire operation fails. The operation releases any assets and dependencies it had already loaded.
+        /// The `Status` of the operation handle is set to <see cref="AsyncOperationStatus.Failed"/> and the `Result` is set to null.
+        ///
+        /// See the [Loading Addressable Assets](xref:addressables-api-load-asset-async) documentation for more details.
+        ///
+        /// See [Operations](xref:addressables-async-operation-handling) for information on handling the asynchronous operations used
+        /// to load Addressable assets.
+        /// </remarks>
+        /// <typeparam name="TObject">The type of the assets.</typeparam>
+        /// <param name="keys">IEnumerable set of keys for the locations.</param>
+        /// <param name="callback">Callback Action that is called per load operation.</param>
+        /// <param name="mode">Method for merging the results of key matches.  See <see cref="MergeMode"/> for specifics</param>
+        /// <param name="releaseDependenciesOnFailure">
+        /// If all matching locations succeed, this parameter is ignored.
+        ///
+        /// When true, if any assets cannot be loaded, the entire operation fails. The operation releases any assets and dependencies it had already loaded.
+        /// The `Status` of the operation handle is set to <see cref="AsyncOperationStatus.Failed"/> and the `Result` is set to null.
+        ///
+        /// When false, if any matching location fails, the `Result` instance in the returned operation handle contains an IList of size equal to the number of
+        /// locations that the operation attempted to load. The entry in the result list corresponding to a location that failed to load is null.
+        /// The entries for locations that successfully loaded are set to a valid TObject. The `Status` of the operation handle is still <see cref="AsyncOperationStatus.Failed"/>
+        /// if any single asset failed to load.
+        ///
+        /// When <paramref name="releaseDependenciesOnFailure"/> is true, you do not need to release the <see cref="AsyncOperationHandle"/> instance on failure.
+        /// When false, you must always release it.
+        /// </param>
+        /// <returns>The operation handle for the request.</returns>
         static public AsyncOperationHandle<IList<TObject>> LoadAssetsAsync<TObject>(IEnumerable keys, Action<TObject> callback,
             Addressables.MergeMode mode = Addressables.MergeMode.Union, bool releaseDependenciesOnFailure = true)
         {
@@ -29,6 +71,14 @@ namespace LitEngine.Addressable
             return thandle;
         }
         
+        /// <summary>
+        /// Instantiate a single object.
+        /// </summary>
+        /// <param name="key">The key of the location of the Object to instantiate.</param>
+        /// <param name="parent">Parent transform for instantiated object.</param>
+        /// <param name="instantiateInWorldSpace">Option to retain world space when instantiated with a parent.</param>
+        /// <param name="trackHandle">If true, Addressables will track this request to allow it to be released via the result object.</param>
+        /// <returns>AsyncOperationHandle that is used to check when the operation has completed. The result of the operation is a GameObject.</returns>
         static public AsyncOperationHandle<GameObject> InstantiateAssetAsync(object key, Transform parent = null, bool instantiateInWorldSpace = false, bool trackHandle = true)
         {
             var thandle = Addressables.InstantiateAsync(key, parent, instantiateInWorldSpace, trackHandle);
