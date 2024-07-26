05e7100a6f13c4c5cfd6727b8b993b544b07b95b
uncaha <34293621+cczxntken@users.noreply.github.com>
2024-07-26 15:15:05 +0800

    add

--- LitEngineUnity/PackageProject/Assets/CustomPackage/com.game.litengine.runtime/Runtime/Loader/Addressables/ResourceseManager.cs
+++ LitEngineUnity/PackageProject/Assets/CustomPackage/com.game.litengine.runtime/Runtime/Loader/Addressables/ResourceseManager.cs
@@ -1,10 +1,21 @@
-namespace LitEngine.Addressables
+using System;
+using System.Collections;
+
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace LitEngine.Addressable
 {
     public class ResourcesManager : MonoManagerGeneric<ResourcesManager>
     {
         protected override void Init()
         {
 
+        }
+        
+        public AsyncOperationHandle<IList<TObject>> LoadAssetsAsync<TObject>(IEnumerable keys, Action<TObject> callback, Addressables.MergeMode mode, bool releaseDependenciesOnFailure)
+        {
+            return Addressables.LoadAssetsAsync<TObject>(keys, callback, mode, releaseDependenciesOnFailure);
         }
     }
 }
